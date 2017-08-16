694d6ed3515ec297d5afbe252d7a22f3b93c4708
Andrés <xlAndres78lx@gmail.com>
2017-08-16 15:59:26 -0500

    1.3

--- MVC/controlador/usuario/usuarioController.php
+++ MVC/controlador/usuario/usuarioController.php
@@ -3,6 +3,9 @@ class usuarioController{
 	function crear(){
 		include_once 'vista/usuario/crear.php';
 	}
+	function postCrear(){
+		extract($_POST);
+	}
 	function listar(){
 		include_once 'vista/usuario/listar.php';
 	}
